4ad5b4f93844e80ac73468fa295ba3a92ae110f9
Yari Andres <yari.andres@gmail.com>
2020-03-02 21:16:24 +0100

    added hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,30 @@
 import React from "react"
 
 import Logo from "./logo"
+import Ceci from "./heroImage"
 
 const Hero = () => {
   return (
     <section className="hero">
-      <Logo/>
-      <h1 className="hero__heading">Learn to Speak Spanish When Suits You</h1>
-      <p className="hero__paragraph">One to one language lessons with native speaking Spanish teachers</p>
+
+      <div className="container-fluid">
+        <Logo/>
+      </div>
+
+      <div className="container">       
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-8">
+            <h1 className="hero__heading">Learn to Speak Spanish <br/> When Suits You</h1>
+            <p className="hero__paragraph">One to one language lessons with <br/> native speaking Spanish <br/> teachers</p>  
+          </div>
+
+          <div className="col-xs-12 col-sm-12 col-md-4">
+            <Ceci/>          
+          </div>
+
+              
+        </div>
+      </div>
     </section>
   )
 }
---
+++ src/components/heroImage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import img from '../images/ceci-img.png'
+
+const Ceci = () => (
+  <div className="hero__img">
+      <img src={img} alt="Ceci Picture"/>
+  </div>
+)
+
+export default Ceci
