785ef07c54985d2d8bde0fc1d342dd4625665951
Yari Andres <yari.andres@gmail.com>
2020-03-04 11:27:36 +0100

    rview section desktop added

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -7,14 +7,15 @@ const Reviews = () => {
     <div className="reviews">
       <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-3 col-lg-8 col-xl-8">
+          <div className="col-sm-12 col-md-3">
             <Customer/>
-          </div>
-            
-          <div className="col-sm-12 col-md-3 col-lg-8 col-xl-8">
-            <p className="paragraph italic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum sus	   pendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
-            <p className="paragraph">Gilliam Patatoe. A real Person</p>
-          </div>
+          </div> 
+
+          <div className="col-sm-12 col-md-9">
+            <p className="reviews__paragraph text-medium italic">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum sus	   pendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+
+            <p className="reviews__name text-medium">Gilliam Patatoe. A real Person</p>
+          </div>  
         </div>
       </div>
     </div>
