96cd4b5f3256e40f9d98e66ad3a4c54d72ce87e8
Yari Andres <yari.andres@gmail.com>
2020-03-08 19:29:31 +0100

    adding benefits section desktop vs

--- src/components/benefit-card.js
+++ src/components/benefit-card.js
@@ -6,24 +6,33 @@ const cards = [
       body: "Spanish for all levels, from beginner, intermediate, to advanced learners",
     },
     { 
-      header: "Spanish for all levels",
-      body: "Spanish for all levels, from beginner, intermediate, to advanced learners"
+      header: "Exam Preparation",
+      body: "Learning designed to increase your performance on tests"
     },
     { 
-      header: "Spanish for all levels",
-      body: "Spanish for all levels, from beginner, intermediate, to advanced learners"
+      header: "Business Spanish",
+      body: "A one-of-a-kind lesson to help you do business in the Spanish-speaking worlds"
     }
   ];
 
 const BenefitsCard = () => {   
   return (
-    <div className="row">
-      {cards.map((item, index) => (        
-        <div className="col-sm-12 col-md-4">
-          <div key={item.index} className="benefits__base">{item.header}</div>
-          
-        </div>    
-      ))}
+
+    <div className="container">
+      <div className="row">
+        {cards.map((item, index) => (        
+          <div key={index} className="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <div  className="benefits__base">            
+              <p className="text-medium text-uppercase benefits__title">
+                {item.header}
+              </p>
+              <p className="text-medium text-center benefits__text">
+                {item.body}
+              </p>
+            </div>          
+          </div>    
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/section-benefits.js
+++ src/components/section-benefits.js
@@ -3,12 +3,9 @@ import BenefitsCard from "./benefit-card";
 
 const Benefits = () => {
   return (
-    <div className="benefits">
-      <div className="container">
-        <h2 className="text-center text-big text-uppercase benefits__heading">learn spanish from anywhere</h2>             
-      
-        <BenefitsCard/>
-      </div>
+    <div className="benefits">   
+      <h2 className="text-center text-big text-uppercase benefits__heading">learn spanish from anywhere</h2>
+      <BenefitsCard/> 
     </div>
   )
 }
